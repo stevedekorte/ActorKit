ccd23ccf553c59c56022702279d915854f2f6ef0
Steve Dekorte <steve@dekorte.com>
2011-08-30 23:28:50 -0700

    Initial Commit

---
+++ ActorKit/ActorKit.h
@@ -0,0 +1,13 @@
+//
+//  ActorKit.h
+//  ActorKit
+//
+//  Created by Steve Dekorte on 20110830.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ActorKit : NSObject
+
+@end
---
+++ ActorKit/ActorKit.m
@@ -0,0 +1,23 @@
+//
+//  ActorKit.m
+//  ActorKit
+//
+//  Created by Steve Dekorte on 20110830.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ActorKit.h"
+
+@implementation ActorKit
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
